c2ad543d6e8999d8bd825d294994868e84eac32d
Alvin-He <shinypeach36@gmail.com>
2022-06-19 20:55:32 -0700

    added fonts

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,16 @@
-// react import 
-import react from "react";
-import reactDom from "react-dom";
+/* eslint-disable @next/next/no-img-element */
 
+//react import 
+import react from 'react';
+import reactDOM from 'react-dom';
+
+// images
+import galLogo from '../public/images/glogo2.svg';
+
+import Title from '../src/components/Title/Title';
+import Navigation from '../src/components/Navigation/Navigation';
+import DropDown from '../src/components/DropDown/DropDown';
+import Link from '../src/components/DropDown/Link/Link';
 
 // react component
 class App extends react.Component {
@@ -18,55 +27,49 @@ class App extends react.Component {
     render() {
         return (
             <div>
-                <TitleBar text = "title bar" />
-            </div>
-        );
-    }
-}
-
-
-class Input extends react.Component {
-    constructor(props) {
-        super(props);
-        this.onKeyDown = this.onKeyDown.bind(this);
-    }
-
-    onKeyDown(event) {
-        if (event.key === 'Enter') {
-            console.log('Enter key pressed');
-            this.props.onSubmit(event.target.value);
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <input type="text" onKeyDown={this.onKeyDown}/>
-            </div>
-        );
-    }
-}
-
-class TitleBar extends react.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: this.props.text
-        };
-        this.update = this.update.bind(this);
-    }
-
-    update(text) {
-        this.setState({
-            text: text
-        });
-    }
-
-    render () {
-        return (
-            <div>
-                <h1>{this.state.text}</h1>
-                <Input onSubmit = {this.update} />
+                <Navigation>
+                    <div className={Navigation.section}>
+                        <DropDown></DropDown>
+                        <DropDown name="About Us">
+                            <Link href="#">Our Team</Link>
+                            <Link href='#'>Media</Link> 
+                            <Link href='#'>Updates</Link>
+                            <Link href='#'>Calendar</Link>
+                        </DropDown>
+                        <DropDown name="Off Season Projects">
+                            <Link href='#'>Trash Bot 2022</Link>
+                            <Link href="#">Tote Bot 2019</Link>
+                        </DropDown>
+                        <DropDown name="Competitions">
+                            <Link href="#">2022: RAPID REACT</Link>
+                            <Link href='#'>2020 &amp; 2021: INFINITE RECHARGE</Link>
+                            <Link href='#'>2019: DEEP SPACE</Link>
+                            <Link href='#'>2018: POWER UP</Link>
+                            <Link href='#'>2017: STEAMWORKS</Link>
+                            <Link href='#'>2016: STRONGHOLD</Link>
+                            <Link href='#'>2015: RECYCLE RUSH</Link>
+                            <Link href='#'>2014: AERIAL ASSIST</Link>
+                            <Link href='#'>2013: Ultimate Ascent{/*no information provided from old website, hoping to fill it in somehow*/}</Link>
+                        </DropDown>
+                    </div>
+                    <div className={Navigation.section}>
+                        <Title>
+                            <div className='spacer'></div>
+                            <div className='fit-content'>
+                                <img className={Title.logo} src={galLogo.src} alt=""></img>
+                            </div>
+                            <div className='fit-content'><p className={Title.text}>4669</p></div>
+                        </Title>
+                    </div>
+                    <div className={Navigation.section}>
+                        <div className='out-link'>
+                            <a href='#'>Resources</a>
+                        </div>
+                        <div className='out-link'>
+                            <a href='#'>Sponsors</a>
+                        </div>
+                    </div>
+                </Navigation>
             </div>
         );
     }
